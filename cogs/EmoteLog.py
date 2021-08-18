a61deb81cbe861d5b199cdef42382cb204d5acf7
adieos <athanokwibisono@gmail.com>
2021-08-18 18:31:54 +0700

    Emote logs

---
+++ cogs/EmoteLog.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+
+class EmoteLog(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_guild_emojis_update(self, guild, before, after):
+        channel = self.client.get_channel(873407182564098048)
+        await channel.send("Hey, an emote just got added !")
+
+
+def setup(client):
+    client.add_cog(EmoteLog(client))
