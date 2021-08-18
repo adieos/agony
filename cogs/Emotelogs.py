b11bca364d4c68f43a05623e9628cd88283414a4
adieos <athanokwibisono@gmail.com>
2021-08-19 06:35:59 +0700

    minor emotelog change

--- cogs/Emotelogs.py
+++ cogs/Emotelogs.py
@@ -11,6 +11,15 @@ class EmoteLog(commands.Cog):
         channel = self.client.get_channel(873407182564098048)
         await channel.send("Hey, an emote just got added ! (or removed.., idk)")
         await channel.send(before)
+        removed = before - after
+        added = after - before
+        if removed is None:
+            await channel.send("An emote just got added!")
+        elif added is None:
+            await channel.send("An emote just got removed!")
+        else:
+            await channel.send("wtf")
+        # use before - after or vice versa !
 
     @commands.command()
     async def amogus(self, ctx):
